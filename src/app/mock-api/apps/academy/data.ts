4c15efe7daecbb19c75f7f7c5297c93456d6c815
Emad Khan <emadkhanqai@gmail.com>
2022-09-30 21:28:40 +0500

    Completed all the 44 assignments.

--- src/app/mock-api/apps/academy/data.ts
+++ src/app/mock-api/apps/academy/data.ts
@@ -560,6 +560,182 @@ export const demoCourseSteps = [
         • Make another list of five usernames called new_users. Make sure one or two of the new usernames are also in the current_users list.
         <br>
         • Loop through the new_users list to see if each new username has already been used. If it has, print a message that the person will need to enter a new username. If a username has not been used, print a message saying that the username is available.
+        <br>
+        • Make sure your comparison is case insensitive. If 'John' has been used, 'JOHN' should not be accepted.
+        </div>
+        `
+    },
+    {
+        order: 31,
+        courseId: '1',
+        title: 'Assignment #32',
+        url: 'https://stackblitz.com/edit/q1-a32?embed=1&file=index.js&hideNavigation=1&theme=dark',
+        subtitle: '',
+        content: `<h2 class="text-2xl sm:text-3xl">Task:</h2>
+        <div>
+        Ordinal Numbers: Ordinal numbers indicate their position in a array, such as 1st or 2nd. Most ordinal numbers end in th, except 1, 2, and 3.
+        <br>
+        • Store the numbers 1 through 9 in a array.
+        <br>
+        • Loop through the array.
+        <br>
+        • Use an if-else chain inside the loop to print the proper ordinal ending for each number. Your output should read "1st 2nd 3rd 4th 5th 6th 7th 8th 9th", and each result should be on a separate line.
+        </div>
+        `
+    },
+    {
+        order: 32,
+        courseId: '1',
+        title: 'Assignment #33',
+        url: 'https://stackblitz.com/edit/q1-a33?embed=1&file=index.js&hideNavigation=1&theme=dark',
+        subtitle: '',
+        content: `<h2 class="text-2xl sm:text-3xl">Task:</h2>
+        <div>
+        Pizzas: Think of at least three kinds of your favorite pizza. Store these pizza names in a array, and then use a for loop to print the name of each pizza.
+        <br>
+        • Modify your for loop to print a sentence using the name of the pizza instead of printing just the name of the pizza. For each pizza you should have one line of output containing a simple statement like I like pepperoni pizza.
+        <br>
+        • Add a line at the end of your program, outside the for loop, that states how much you like pizza. The output should consist of three or more lines about the kinds of pizza you like and then an additional sentence, such as I really love pizza!
+        </div>
+        `
+    },
+    {
+        order: 33,
+        courseId: '1',
+        title: 'Assignment #34',
+        url: 'https://stackblitz.com/edit/q1-a34?embed=1&file=index.js&hideNavigation=1&theme=dark',
+        subtitle: '',
+        content: `<h2 class="text-2xl sm:text-3xl">Task:</h2>
+        <div>
+        Animals: Think of at least three different animals that have a common characteristic. Store the names of these animals in a list, and then use a for loop to print out the name of each animal.
+        <br>
+        • Modify your program to print a statement about each animal, such as A dog would make a great pet.
+        <br>
+        • Add a line at the end of your program stating what these animals have in common. You could print a sentence such as Any of these animals would make a great pet!
+        </div>
+        `
+    },
+    {
+        order: 34,
+        courseId: '1',
+        title: 'Assignment #35',
+        url: 'https://stackblitz.com/edit/q1-a35?embed=1&file=index.js&hideNavigation=1&theme=dark',
+        subtitle: '',
+        content: `<h2 class="text-2xl sm:text-3xl">Task:</h2>
+        <div>
+        T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the message printed on it. Call the function.
+        </div>
+        `
+    },
+    {
+        order: 35,
+        courseId: '1',
+        title: 'Assignment #36',
+        url: 'https://stackblitz.com/edit/q1-a36?embed=1&file=index.js&hideNavigation=1&theme=dark',
+        subtitle: '',
+        content: `<h2 class="text-2xl sm:text-3xl">Task:</h2>
+        <div>
+        Large Shirts: Modify the make_shirt() function so that shirts are large by default with a message that reads I love JavaScript. Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.
+        </div>
+        `
+    },
+    {
+        order: 36,
+        courseId: '1',
+        title: 'Assignment #37',
+        url: 'https://stackblitz.com/edit/q1-a37?embed=1&file=index.js&hideNavigation=1&theme=dark',
+        subtitle: '',
+        content: `<h2 class="text-2xl sm:text-3xl">Task:</h2>
+        <div>
+        Cities: Write a function called describe_city() that accepts the name of a city and its country. The function should print a simple sentence, such as Karachi is in Pakistan. Give the parameter for the country a default value. Call your function for three different cities, at least one of which is not in the default country.
+        </div>
+        `
+    },
+    {
+        order: 37,
+        courseId: '1',
+        title: 'Assignment #38',
+        url: 'https://stackblitz.com/edit/q1-a38?embed=1&file=index.js&hideNavigation=1&theme=dark',
+        subtitle: '',
+        content: `<h2 class="text-2xl sm:text-3xl">Task:</h2>
+        <div>
+        City Names: Write a function called city_country() that takes in the name of a city and its country. The function should return a string formatted like this:
+<br>
+        "Lahore, Pakistan"
+        <br>
+        Call your function with at least three city-country pairs, and print the value that’s returned.
+        </div>
+        `
+    },
+    {
+        order: 38,
+        courseId: '1',
+        title: 'Assignment #39',
+        url: 'https://stackblitz.com/edit/q1-a39?embed=1&file=index.js&hideNavigation=1&theme=dark',
+        subtitle: '',
+        content: `<h2 class="text-2xl sm:text-3xl">Task:</h2>
+        <div>
+        Album: Write a function called make_album() that builds a Object describing a music album. The function should take in an artist name and an album title, and it should return a Object containing these two pieces of information. Use the function to make three dictionaries representing different albums. Print each return value to show that Objects are storing the album information correctly. Add an optional parameter to make_album() that allows you to store the number of tracks on an album. If the calling line includes a value for the number of tracks, add that value to the album’s Object. Make at least one new function call that includes the number of tracks on an album.
+        </div>
+        `
+    },
+    {
+        order: 39,
+        courseId: '1',
+        title: 'Assignment #40',
+        url: 'https://stackblitz.com/edit/q1-a40?embed=1&file=index.js&hideNavigation=1&theme=dark',
+        subtitle: '',
+        content: `<h2 class="text-2xl sm:text-3xl">Task:</h2>
+        <div>
+        Magicians: Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of each magician in the array.
+        </div>
+        `
+    },
+    {
+        order: 40,
+        courseId: '1',
+        title: 'Assignment #41',
+        url: 'https://stackblitz.com/edit/q1-a41?embed=1&file=index.js&hideNavigation=1&theme=dark',
+        subtitle: '',
+        content: `<h2 class="text-2xl sm:text-3xl">Task:</h2>
+        <div>
+        Great Magicians: Start with a copy of your program from Exercise 39. Write a function called make_great() that modifies the array of magicians by adding the phrase the Great to each magician’s name. Call show_magicians() to see that the list has actually been modified.
+        </div>
+        `
+    },
+    {
+        order: 41,
+        courseId: '1',
+        title: 'Assignment #42',
+        url: 'https://stackblitz.com/edit/q1-a42?embed=1&file=index.js&hideNavigation=1&theme=dark',
+        subtitle: '',
+        content: `<h2 class="text-2xl sm:text-3xl">Task:</h2>
+        <div>
+        Unchanged Magicians: Start with your work from Exercise 40. Call the function make_great() with a copy of the array of magicians’ names. Because the original array will be unchanged, return the new array and store it in a separate array. Call show_magicians() with each array to show that you have one array of the original names and one array with the Great added to each magician’s name.
+        </div>
+        `
+    },
+    {
+        order: 42,
+        courseId: '1',
+        title: 'Assignment #43',
+        url: 'https://stackblitz.com/edit/q1-a43?embed=1&file=index.js&hideNavigation=1&theme=dark',
+        subtitle: '',
+        content: `<h2 class="text-2xl sm:text-3xl">Task:</h2>
+        <div>
+        Sandwiches: Write a function that accepts a array of items a person wants on a sandwich. The function should have one parameter that collects as many items as the function call provides, and it should print a summary of the sandwich that is being ordered. Call the function three times, using a different number of arguments each time.
+        </div>
+        `
+    },
+    {
+        order: 43,
+        courseId: '1',
+        title: 'Assignment #44',
+        url: 'https://stackblitz.com/edit/q1-a44?embed=1&file=index.js&hideNavigation=1&theme=dark',
+        subtitle: '',
+        content: `<h2 class="text-2xl sm:text-3xl">Task:</h2>
+        <div>
+        Cars: Write a function that stores information about a car in a Object. The function should always receive a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments. Call the function with the required information and two other name-value pairs, such as a color or an optional feature. Print the Object that’s returned to make sure all the information was stored correctly.
         </div>
         `
     },
