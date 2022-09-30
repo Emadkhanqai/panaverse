79538c04b6f5bf2130e2ddc2ec2564ab51c8cab1
Emad Khan <emadkhanqai@gmail.com>
2022-09-30 06:43:06 +0500

    navigation settled for mobile.

--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -3,28 +3,100 @@ import { FuseNavigationItem } from '@fuse/components/navigation';
 
 export const defaultNavigation: FuseNavigationItem[] = [
     {
-        id: 'example',
-        title: 'Example',
+        id: 'dashboard',
+        title: 'Analytics',
         type: 'basic',
-        icon: 'heroicons_outline:chart-pie',
-        link: '/example'
+        icon: 'heroicons_solid:presentation-chart-line',
+        link: '/analytics'
+    }
+    ,
+    {
+        id: 'assignment',
+        title: 'Assignments',
+        type: 'basic',
+        icon: 'heroicons_solid:cloud',
+        link: '/assignment'
+    }
+    ,
+    {
+        id: 'learn',
+        title: 'Learn Blockchain, Web 3.0 and Metaverse',
+        type: 'basic',
+        icon: 'heroicons_solid:academic-cap',
+        link: '/learn'
+    }
+    ,
+    {
+        id: 'help',
+        title: 'Help Center',
+        type: 'basic',
+        icon: 'heroicons_solid:light-bulb',
+        link: '/help-center'
     }];
 export const compactNavigation: FuseNavigationItem[] = [
     {
-        id: 'example',
-        title: 'Example',
+        id: 'dashboard',
+        title: 'Analytics',
         type: 'basic',
-        icon: 'heroicons_outline:chart-pie',
-        link: '/example'
+        icon: 'heroicons_solid:presentation-chart-line',
+        link: '/analytics'
+    }
+    ,
+    {
+        id: 'assignment',
+        title: 'Assignments',
+        type: 'basic',
+        icon: 'heroicons_solid:cloud',
+        link: '/assignment'
+    }
+    ,
+    {
+        id: 'learn',
+        title: 'Learn Blockchain, Web 3.0 and Metaverse',
+        type: 'basic',
+        icon: 'heroicons_solid:academic-cap',
+        link: '/learn'
+    }
+    ,
+    {
+        id: 'help',
+        title: 'Help Center',
+        type: 'basic',
+        icon: 'heroicons_solid:light-bulb',
+        link: '/help-center'
     }
 ];
 export const futuristicNavigation: FuseNavigationItem[] = [
     {
-        id: 'example',
-        title: 'Example',
+        id: 'dashboard',
+        title: 'Analytics',
         type: 'basic',
-        icon: 'heroicons_outline:chart-pie',
-        link: '/example'
+        icon: 'heroicons_solid:presentation-chart-line',
+        link: '/analytics'
+    }
+    ,
+    {
+        id: 'assignment',
+        title: 'Assignments',
+        type: 'basic',
+        icon: 'heroicons_solid:cloud',
+        link: '/assignment'
+    }
+    ,
+    {
+        id: 'learn',
+        title: 'Learn Blockchain, Web 3.0 and Metaverse',
+        type: 'basic',
+        icon: 'heroicons_solid:academic-cap',
+        link: '/learn'
+    }
+    ,
+    {
+        id: 'help',
+        title: 'Help Center',
+        type: 'basic',
+        icon: 'heroicons_solid:light-bulb',
+        link: '/help-center'
     }
 ];
 export const horizontalNavigation: FuseNavigationItem[] = [
